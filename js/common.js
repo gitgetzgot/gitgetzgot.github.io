28c269ef25f3d617ba2648ab377d5d7cee5075c6
gitgetzgot <30385054+gitgetzgot@users.noreply.github.com>
2017-08-16 19:34:16 +0530

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,27 @@
+﻿function ShowTab(tabNo, Total){
+    var tabId = 'tab' + tabNo;
+    var tabContentId = 'tabContent' + tabNo;
+    document.getElementById(tabId).className = "selected";
+    document.getElementById(tabContentId).style.display = "block";
+    
+    for(i=1;i<=Total;i++){
+        if(i != tabNo){
+            tabId = 'tab' + i;
+            tabContentId = 'tabContent' + i;
+            document.getElementById(tabId).className = "";
+            document.getElementById(tabContentId).style.display = "none";
+        }
+    } 
+    
+    goBackCount = 2;
+}
+
+function GoBack(){ 
+    var x = -1;
+        x = x * goBackCount;        
+    history.go(x);
+}
+
+function ConfirmDelete() {
+  return confirm("Are you SURE you want to delete this article?");
+};
